f471130179728e81ef0773d531c2ba0e5c78924a
followkarthick <followkarthick@gmail.com>
2020-03-04 17:44:25 +0530

    1 file modified

--- searchOptions.js
+++ searchOptions.js
@@ -12,75 +12,24 @@ class SearchOptions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      radioValue: 'LevelId',
-      radioLoadedChange: 'Loaded',
-      radioComparisonValue: 'Across',
-      levelIdEanValue: '',
-      cashcheckBoxValue: '',
-      deletecheckBoxValue: '',
-      searchValue: '',
-output:{
-levelIDorEAN:'',
-searchValue:'',
-loadedopt:'',
-cashopt:''
-}
+      searchValue: '',      
 
     }
   }
 
   searchClick = event => {
-    
-let out = {
-  
-levelIDorEAN:this.state.radioValue,
-searchValue:this.state.levelIdEanValue,
-loadedOpt:this.state.radioLoadedValue,
-compareOpt:this.state.radioComparisonValue,
-cashOpt:[this.state.cashcheckBoxValue,this.state.deletecheckBoxValue]
-}
 
-
-      this.props.searchUpdated(out);
-    
-    
+    let out = {
+      searchValue: this.state.levelIdEanValue,
+    }
+    this.props.searchUpdated(out);
   }
-
   levelIdChange = event => {
     const value = event.target.value;
     this.setState({ levelIdEanValue: value });
   }
 
-  cashCheckBoxChange = event => {
-    if (event.target.checked) {
-      const value = event.target.value;
-      this.setState({ cashcheckBoxValue: value });
-    } else {
-      this.setState({ cashcheckBoxValue: '' });
-    }
-  }
-  deleteCheckBoxChange = event => {
-    if (event.target.checked) {
-      const value = event.target.value;
-      this.setState({ deletecheckBoxValue: value });
-    } else {
-      this.setState({ deletecheckBoxValue: '' });
-    }
-  }
-  radioChange = (event) => {
-    const value = event.target.value;
-    this.setState({ radioValue: value });
-  }
 
-  radioLoadedChange = (event) => {
-    const value = event.target.value;
-    this.setState({ radioLoadedValue: value });
-  }
-
-  radioComparisonChange = (event) => {
-    const value = event.target.value;
-    this.setState({ radioComparisonValue: value });
-  }
 
   render() {
     return (
@@ -96,24 +45,6 @@ cashOpt:[this.state.cashcheckBoxValue,this.state.deletecheckBoxValue]
           </CardBox>
 
 
-          <CardBox>
-            <CardBoxHeader> Pricing Loaded Options</CardBoxHeader>
-
-            <Radio name="loaded" value="Pricing Loaded only" defaultChecked="defaultChecked" onChange={this.radioLoadedChange} />      <Radio name="loaded" value="Pricing Loaded with inheritance" onChange={this.radioLoadedChange} />
-
-          </CardBox>
-          <CardBox>
-            <CardBoxHeader> Hierarchy Comparison Direction </CardBoxHeader>
-            <Radio name="comparison" value="Compare Across Hierarchy" defaultChecked="defaultChecked" onChange={this.radioComparisonChange} />
-            <Radio name="comparison" value="Compare Down Hierarchy" onChange={this.radioComparisonChange} />
-          </CardBox>
-
-          <CardBox>
-            <CardBoxHeader> Pricing Options</CardBoxHeader>
-            <Checkbox type="checkbox" value="Cash" className="margin" onChange={this.cashCheckBoxChange} />
-            <Checkbox type="checkbox" value="Delete" className="margin" onChange={this.deleteCheckBoxChange} />
-          </CardBox>
-
 
         </SearchContainer>
         <SubHeader>Service Group Types </SubHeader>
@@ -123,7 +54,7 @@ cashOpt:[this.state.cashcheckBoxValue,this.state.deletecheckBoxValue]
           <Button type="submit" onClick={this.searchClick} />
         </ButtonMargin>
         <SearchResults>
-          
+
         </SearchResults>
       </div>
     );
